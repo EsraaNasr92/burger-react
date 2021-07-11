19c81377a488a5ffaeb715ccb349e49dae2d4276
EsraaNasr92 <esraanasr706@gmail.com>
2021-07-11 15:35:09 +0200

    Burger app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Burger from './compontents/Burger.js'
+
+function App() {
+  return (
+    <div className="burgerContent">
+      <Burger />
+    </div>
+  );
+}
+
+export default App;
